5f8c87052ed280e21ff08ff01a4dcec8da11d6be
Klaudio Picaku <picaku.klaudio@yahoo.com>
2023-04-26 18:24:23 +0200

    first commit

---
+++ src/main/java/com/siw/uniroma3/it/siw_lavendetta/Siw2LaVendettaApplication.java
@@ -0,0 +1,13 @@
+package com.siw.uniroma3.it.siw_lavendetta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Siw2LaVendettaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Siw2LaVendettaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/siw/uniroma3/it/siw_lavendetta/Siw2LaVendettaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siw.uniroma3.it.siw_lavendetta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Siw2LaVendettaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
